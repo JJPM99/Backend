d76964d80d5e17d26f878d897b8d8cb935dd7180
José Javier Puerta Mariñez <64931824+JJPM99@users.noreply.github.com>
2024-04-18 22:46:06 -0600

    Configuración Inicial Netflix Zuul
    Primera parte de la tarea#4 en el que empezamos a utilizar la herramienta de Netflix Zuul en nuestro proyecto de Spring Boot.

--- springboot-service-celulares1/src/main/java/com/microservice/celulares1/controllers/CelularController.java
+++ springboot-service-celulares1/src/main/java/com/microservice/celulares1/controllers/CelularController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,6 +22,8 @@ import com.microservice.celulares1.service.CelularService;
 
 @RestController
 public class CelularController {
+	@Autowired
+	private Environment env;
 	@Autowired 
 	private  CelularService service;
 	
@@ -30,7 +33,7 @@ public class CelularController {
 	@GetMapping("/list")
    public List<Celular> list(){
 	   return service.findAll().stream().map(cel -> {
-	   cel.setPort(port);
+	   cel.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 	   return cel;
 	}).collect(Collectors.toList());
    }
