fbe1a25b76bf772b30723569c55dea32f57753c5
José Javier Puerta Mariñez <64931824+JJPM99@users.noreply.github.com>
2024-04-25 17:48:24 -0600

    Usuarios
    tarea#5 de nuestro proyecto de Spring Boot donde creamos el microservicio de Usuarios.

---
+++ srpingboot-service-usuarios/src/main/java/com/microservice/usuarios/SrpingbootServiceUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingbootServiceUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingbootServiceUsuariosApplication.class, args);
+	}
+
+}
